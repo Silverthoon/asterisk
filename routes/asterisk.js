8bb9391bc4b5c36bbf8ba53e21965062f47765f5
Meijink <junada99@gmail.com>
2021-03-26 02:04:21 +0100

    Update files

--- routes/asterisk.js
+++ routes/asterisk.js
@@ -5,10 +5,10 @@ const paginate = require('express-paginate');
 const { Op } = require("sequelize");
 const moment = require('moment');
 
-/* GET dashboard page. */
+/* Dashboard------------------------------------------------------------------------------------------------------------- */
 router.get('/dashboard', function (req, res, next) {
   async function loadCalls() {
-    var calls = await CallList.findAndCountAll({order: [['accountcode', 'ASC'],],},);
+    var calls = await CallList.findAndCountAll({order: [['accountcode', 'ASC']],},);
     const itemCount = calls.count;
     const pageCount = Math.ceil(calls.count / req.query.limit);
     if (req.session.username){
@@ -23,41 +23,130 @@ router.get('/dashboard', function (req, res, next) {
   loadCalls();
 });
 
+
+/* Billing--------------------------------------------------------------------------------------------------------------- */
 router.get('/billing', function(req, res, next) {
   res.render('billing', { title: 'Billing' });
 });
 
 router.post('/billing', function (req, res, next) {
-  async function bill() {
+  if(req.body.to != "")
     var to = new Date(req.body.to+" 23:59:59");
+  if(req.body.to != "")
     var from = new Date(req.body.from);
-    var bills = await CallList.findAll({
-      where : {
-        start : {
-          [Op.gte] : from
-        },
-        end : {
-          [Op.lte] : to
-        },
-        disposition : {
-          [Op.eq] : 'ANSWERED'
-        },
-        dst : {
-          [Op.eq] : 8001
+  var recepient = req.body.recepient;
+  var src = req.body.src;
+  if((from) && (to) && (recepient)){
+    async function billFromTo() {
+      console.log('1');
+      var bills = await CallList.findAll({order: [['accountcode', 'ASC']],
+        where : {
+          start : {
+            [Op.gte] : from
+          },
+          end : {
+            [Op.lte] : to
+          },
+          disposition : {
+            [Op.eq] : 'ANSWERED'
+          },
+          dst : {
+            [Op.eq] : recepient
+          }
         }
+      });
+      var totalCost = 0;
+      var totalDuration = 0;
+      bills.forEach((val,index) => {
+        totalDuration = totalDuration + bills[index].billsec;
+      });
+      totalCost = totalDuration * 2;
+      res.render('bills', { title : 'Bills', totalCost : totalCost, totalDuration : totalDuration, from : from, to : to, dst : recepient, username : req.session.username});
+    }
+    billFromTo();
+  }else if((recepient) && (src)){
+      async function billReceipSrc() {
+        console.log('2');
+        var bills = await CallList.findAll({order: [['accountcode', 'ASC']],
+          where : {
+            src : {
+              [Op.eq] : src
+            },
+            dst : {
+              [Op.eq] : recepient
+            }
+          }
+        });
+        var totalCost = 0;
+        var totalDuration = 0;
+        bills.forEach((val,index) => {
+          totalDuration = totalDuration + bills[index].billsec;
+        });
+        totalCost = totalDuration * 2;
+        res.render('bills', { title : 'Bills', totalCost : totalCost, totalDuration : totalDuration, src : src, recepient : recepient, username : req.session.username});
       }
-    });
-    var totalCost = 0;
-    var totalDuration = 0;
-    bills.forEach((val,index) => {
-      totalDuration = totalDuration + bills[index].billsec;
-    });
-    totalCost = totalDuration * 2;
-    res.render('billing', { title : 'Billing', totalCost : totalCost, totalDuration : totalDuration, from : from, to : to, username : req.session.username});
-  }
-  bill();
+      billReceipSrc();
+  }else if(recepient){
+    async function billReceip() {
+      console.log('3');
+      var bills = await CallList.findAll({order: [['accountcode', 'ASC']],
+        where : {
+          dst : {
+            [Op.eq] : recepient
+          }
+        }
+      });
+      var totalCost = 0;
+      var totalDuration = 0;
+      bills.forEach((val,index) => {
+        totalDuration = totalDuration + bills[index].billsec;
+      });
+      totalCost = totalDuration * 2;
+      res.render('bills', { title : 'Bills', totalCost : totalCost, totalDuration : totalDuration, recepient : recepient, username : req.session.username});
+    }
+    billReceip();
+  }/*else if(src){
+    async function billSrc() {
+      console.log('4');
+      var bills = await CallList.findAll({order: [['accountcode', 'ASC']],
+        where : {
+          src : {
+            [Op.eq] : src
+          }
+        }
+      });
+      var totalCost = 0;
+      var totalDuration = 0;
+      bills.forEach((val,index) => {
+        totalDuration = totalDuration + bills[index].billsec;
+      });
+      totalCost = totalDuration * 2;
+      res.render('bills', { title : 'Bills', totalCost : totalCost, totalDuration : totalDuration, src : src, username : req.session.username});
+    }
+    billSrc();
+  }*/
 });
 
+
+/* Bills---------------------------------------------------------------------------------------------------------------- */
+
+router.get('/bills', function(req, res, next) {
+  res.redirect('/asterisk/dashboard');
+});
+
+/* Contact us------------------------------------------------------------------------------------------------------------- */
+
+router.get('/contact', function(req, res, next) {
+  res.redirect('/asterisk/dashboard');
+});
+
+/* Settings------------------------------------------------------------------------------------------------------------- */
+
+router.get('/settings', function(req, res, next) {
+  res.redirect('/asterisk/dashboard');
+});
+
+/* Logout--------------------------------------------------------------------------------------------------------------- */
 router.get('/logout', function(req, res, next) {
   if (req.session) {
     // delete session object
